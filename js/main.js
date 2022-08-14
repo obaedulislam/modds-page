f1cdb8a098b7f9ddae791a967ef4c2b207f41943
obaedulislam <obaed.cse@gmail.com>
2022-08-14 18:18:50 +0600

    Project basic part done

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoHeight:true,
+    lazyLoad:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
